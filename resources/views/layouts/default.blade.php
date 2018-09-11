89d843c99bceea151ecedc0641c947874c7b0808
2018 <niechen1510@gmail.com>
2018-09-11 07:54:46 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title','Sample') - Laravel 教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+   @include('layouts._header')
+
+    <div class="container">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
